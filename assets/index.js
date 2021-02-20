5c2bf11256147e653e0cdfde0ec3cac1b4d37fee
Stacy Betts <stacyo23@yahoo.com>
2021-02-20 13:33:02 -0600

    created question list

---
+++ assets/index.js
@@ -0,0 +1,31 @@
+//array of questions to be rendered to page
+
+var questions = [
+    {
+        title: "Which of these is NOT considered a type of data?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }, 
+    {
+        title: "Arrays must be enclosed in ___",
+        choices: ["curly brackets", "parenthesis","nothing", "square brackets" ],
+        answer: "square brackets"
+    }, 
+    {
+        title: "Which symbol is required to target an id element in CSS?",
+        choices: ["#", "*", ".", "/"], 
+        answer: "#"
+    }, 
+    {
+        title: "Which of the following is not a coding language?", 
+        choices: ["Javascript", "goat yoga", "Jquery", "HTML/CSS"],
+        answer: "goat yoga"
+    }, 
+    {
+        title: "Strings are enclosed in ___",
+        choices: ["parenthesis", "curly brackets", "a circle", "quotation marks"],
+        answer: "quotation marks"
+    }
+]
+
+
